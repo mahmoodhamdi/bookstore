c1a53b6f78a365481a2d5da2b037824106318f75
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-22 20:45:09 +0200

    add custom app bar

---
+++ lib/featuers/home/persention/view/widgets/custom_appbar.dart
@@ -0,0 +1,31 @@
+import 'package:bookstore/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search_outlined,
+              size: 25,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
