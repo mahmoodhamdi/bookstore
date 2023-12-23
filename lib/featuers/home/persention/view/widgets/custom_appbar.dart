42e9ae39411d331eb89cb69295f24efa1a6d2c9a
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-24 01:59:05 +0200

    create style file and add text widget

--- lib/featuers/home/persention/view/widgets/custom_appbar.dart
+++ lib/featuers/home/persention/view/widgets/custom_appbar.dart
@@ -10,7 +10,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 50),
+      padding: const EdgeInsets.only(top: 60, bottom: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
--- lib/featuers/home/persention/view/widgets/home_view_body.dart
+++ lib/featuers/home/persention/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookstore/core/utils/text_styles.dart';
 import 'package:bookstore/featuers/home/persention/view/widgets/custom_appbar.dart';
 import 'package:bookstore/featuers/home/persention/view/widgets/featuers_books_listview.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        Expanded(child: FeaturedBooksListView()),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Best Seller",
+            style: TextStyles.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
