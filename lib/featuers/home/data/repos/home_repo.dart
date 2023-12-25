d22f3c7ace741041f4684adb257e43f35afd8e06
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-25 13:47:02 +0200

    update home repo with Either class

--- lib/featuers/home/data/repos/home_repo.dart
+++ lib/featuers/home/data/repos/home_repo.dart
@@ -1,5 +1,8 @@
-abstract class HomeRepo {
-  featchBestSellerBooks();
-  featchFeatuerdBooks();
+import 'package:bookstore/core/errors/failures.dart';
+import 'package:bookstore/featuers/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
+abstract class HomeRepo {
+  Future<Either<Failure,List<BookModel>>> featchBestSellerBooks();
+    Future<Either<Failure, List<BookModel>>>  featchFeatuerdBooks();
 }
